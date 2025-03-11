9f0e2d6341bf42df858f645449bce36d414548f3
Avi Banerjee <avibanerjee2000@gmail.com>
2025-03-11 12:26:53 +0000

    layout for auth

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,4 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-export default {
+export const defaultPrettier = {
   plugins: ["prettier-plugin-tailwindcss"],
 };
---
+++ src/app/components/Home/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return <main className="flex-center h-screen">
+    <h1 className="h1">
+      Stache - The only storage solution you need.
+    </h1>
+  </main>
+} 
